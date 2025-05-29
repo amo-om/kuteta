6a3f14f6d5a125db51a0d52bcad539064619b8e2
Kwame Yeboah <funcount673@gmail.com>
2025-05-29 02:17:44 -0500

    Quartz sync: May 29, 2025, 2:17 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Kuteta",
-    pageTitleSuffix: "Test",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
